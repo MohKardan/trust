b0ad6f05af0e464025e4a0fc3fa7e8d92b8be5f4
MohKardan <45781438+MohKardan@users.noreply.github.com>
2023-06-02 18:12:38 +0430

    کلاه خاکستری
    بازیکن ناشناس اضافه شد
    انتخاب اسلاید پایین صفحه و تعدادی از نوشته ها حذف و یا کامنت شد

--- js/main.js
+++ js/main.js
@@ -1,61 +1,57 @@
 var slideshow, slideSelect;
-window.onload = function(){
+window.onload = function () {
+  // PRELOADER
+  Q.all([
+    Loader.loadAssets(Loader.manifestPreload),
+    Words.convert("words.html"),
+  ]).then(function () {
+    // CHANGE DOM
+    document.body.removeChild($("#preloader"));
+    $("#main").style.display = "block";
+    $("#footer").style.display = "block";
 
-	// PRELOADER
-	Q.all([
-		Loader.loadAssets(Loader.manifestPreload),
-		Words.convert("words.html")
-	]).then(function(){
+    // Slideshow
+    slideshow = new Slideshow({
+      dom: $("#slideshow"),
+      slides: SLIDES,
+    });
 
-		// CHANGE DOM
-		document.body.removeChild($("#preloader"));
-		$("#main").style.display = "block";
-		$("#footer").style.display = "block";
+    // Slide Select
+    // slideSelect = new SlideSelect({
+    //   dom: $("#select"),
+    //   slides: SLIDES,
+    // });
+    //slideSelect.dom.style.display = "none";
 
-		// Slideshow
-		slideshow = new Slideshow({
-			dom: $("#slideshow"),
-			slides: SLIDES
-		});
+    subscribe("start/game", function () {
+      //slideSelect.dom.style.display = "block";
+      $("#translations").style.display = "none";
 
-		// Slide Select
-		slideSelect = new SlideSelect({
-			dom: $("#select"),
-			slides: SLIDES
-		});
-		slideSelect.dom.style.display = "none";
-		subscribe("start/game", function(){
-			slideSelect.dom.style.display = "block";
-			$("#translations").style.display = "none";
+      // [FOR DEBUGGING]
+      publish("slideshow/next");
+      //publish("slideshow/scratch", ["credits"]);
+    });
 
-			// [FOR DEBUGGING]
-			publish("slideshow/next");
-			//publish("slideshow/scratch", ["credits"]);
+    // SOUND
+    var _soundIsOn = true;
+    $("#sound").onclick = function () {
+      _soundIsOn = !_soundIsOn;
+      Howler.mute(!_soundIsOn);
+      $("#sound").setAttribute("sound", _soundIsOn ? "on" : "off");
+    };
 
-		});
+    // LOAD REAL THINGS
+    Loader.loadAssets(
+      Loader.manifest,
+      function () {
+        publish("preloader/done");
+      },
+      function (ratio) {
+        publish("preloader/progress", [ratio]);
+      }
+    );
 
-		// SOUND
-		var _soundIsOn = true;
-		$("#sound").onclick = function(){
-			_soundIsOn = !_soundIsOn;
-			Howler.mute(!_soundIsOn);
-			$("#sound").setAttribute("sound", _soundIsOn?"on":"off");
-		};
-
-		// LOAD REAL THINGS
-		Loader.loadAssets(
-			Loader.manifest,
-			function(){
-				publish("preloader/done");
-			},
-			function(ratio){
-				publish("preloader/progress", [ratio]);
-			}
-		);
-
-		// First slide!
-		slideshow.nextSlide();
-
-	});
-
+    // First slide!
+    slideshow.nextSlide();
+  });
 };
